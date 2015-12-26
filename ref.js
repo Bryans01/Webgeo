ae11994bba3586d383549238e5327d2062d2fd82
Bryans01 <Steve.h.bryan@gmail.com>
2015-12-26 13:18:55 +0200

    Initial commit

---
+++ ref.js
@@ -0,0 +1,5 @@
+myData = [
+[-29.9773392,30.9618181, 'Sapref, 4133, South Africa'],
+[-29.8586804,31.0218404, 'Durban, South Africa'],
+[-26.8101899,27.8277254, 'Sasolburg, South Africa']
+];
---
+++ where.js
@@ -0,0 +1,14 @@
+myData = [
+[-29.6006068,30.3794118, 'Pietermaritzburg, South Africa'],
+[-29.8586804,31.0218404, 'Durban, South Africa'],
+[-26.2041028,28.0473051, 'Johannesburg, South Africa'],
+[-33.9248685,18.4240553, 'Cape Town, South Africa'],
+[51.5073509,-0.1277583, 'London, UK'],
+[48.856614,2.3522219, 'Paris, France'],
+[52.5200066,13.404954, 'Berlin, Germany'],
+[52.5200066,13.404954, 'Berlin, Germany'],
+[-25.7478676,28.2292712, 'Pretoria, South Africa'],
+[-28.7807276,32.0382856, 'Richards Bay, South Africa'],
+[-33.3106292,26.5255946, 'Grahamstown, South Africa'],
+[-33.9321045,18.860152, 'Stellenbosch, South Africa']
+];
